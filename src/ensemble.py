e26eb98fe3188e911ed59aeafcc51a55204ac3a3
mifart <neromifart@gmail.com>
2026-03-29 21:09:36 +0300

    Create ensemble.py

---
+++ src/ensemble.py
@@ -0,0 +1,4 @@
+class EnsembleModel:
+      def __init__(self): self.lgbm_model = None; self.cat_model = None
+            def load_models(self): pass
+                  def predict(self, x): return [100.0]
